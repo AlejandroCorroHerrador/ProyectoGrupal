bfdb18bf3b0a1906872f510af8181949a715680c
alu10513774 <alucor3774@ieselcaminas.org>
2024-03-25 20:59:34 +0100

    Commit 8. Problemas emn el app.properties

--- src/main/java/models/repositories/ProfesorRepository.java
+++ src/main/java/models/repositories/ProfesorRepository.java
@@ -4,6 +4,7 @@ import models.entities.Profesor;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+//Repositorio
 @Repository
 public interface ProfesorRepository extends JpaRepository<Profesor, Long> {
 }
